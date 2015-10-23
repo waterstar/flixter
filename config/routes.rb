1b17e245d0de413c72b5d205f728a17b803963a3
suising <suising@waterstar.sg>
2015-10-23 22:31:30 +0000

    add sections model and form to add it

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,8 @@ Flixter::Application.routes.draw do
   devise_for :users
   resources :courses, :only => [:index, :show]
   namespace :instructor do
-    resources :courses, :only => [:new, :create, :show]
+    resources :courses, :only => [:new, :create, :show] do
+      resources :sections, :only => [:new, :create]
+    end
   end
 end
