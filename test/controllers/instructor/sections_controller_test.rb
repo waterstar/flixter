a90e2e2305047d19b612f03a0f553f53b760fc79
suising <suising@waterstar.sg>
2015-10-26 17:54:36 +0000

    permissions check for instructor to add new sections and courses

--- test/controllers/instructor/sections_controller_test.rb
+++ test/controllers/instructor/sections_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class Instructor::SectionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "post instructor section login" do
+    course = FactoryGirl.create(:course)
+    user = FactoryGirl.create(:user)
+    sign_in user do
+      post :create, :course_id => course.id, "section" => { :title => "FactoryGirl test"}
+      assert_redirected_to instructor_course_path(course)
+    end
+    sign_out user
+  end
+
+  test "post instructor section without login" do
+    course = FactoryGirl.create(:course)
+
+    post :create, :course_id => course.id, "section" => { :title => "FactoryGirl test 2"}
+    assert_response :unauthorized
+  end
+
+  test "only user who created the course can add sections to the course" do
+  end
+
 end
