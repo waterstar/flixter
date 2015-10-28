a0488f948c0351d840c7c1a284d034d113f1f665
suising <suising@waterstar.sg>
2015-10-28 14:29:40 +0000

    add image uploaders

--- Gemfile
+++ Gemfile
@@ -42,6 +42,13 @@ gem 'simple_form'
 
 gem 'factory_girl_rails', "~> 4.0"
 
+gem 'carrierwave'
+
+gem "figaro", ">= 1.0.0"
+
+gem "fog", "~> 1.3.1"
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
 	belongs_to :user
-    has_many :sections
+  has_many :sections
 
 	validates :title, :presence => true
 	validates :description, :presence => true
