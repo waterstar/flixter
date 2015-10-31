01adb1fe728d37c2c893fcb9094737004a9310c5
suising <suising@waterstar.sg>
2015-11-01 02:49:35 +0800

    check user enrollments before showing lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,10 @@
 class LessonsController < ApplicationController
-  def show
+  before_action :authenticate_user!
 
+  def show
+      unless current_user.enrolled_in?(current_lesson.section.course)
+        redirect_to course_path(current_lesson.section.course), :alert => 'Enroll the course to view lesson' 
+      end
   end
 
   private
