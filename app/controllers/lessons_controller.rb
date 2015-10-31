970b15bb8a8557d94f88539e95d9b51fdf064450
suising <suising@waterstar.sg>
2015-11-01 03:08:05 +0800

    minor refactoring

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,8 +2,8 @@ class LessonsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-      unless current_user.enrolled_in?(current_lesson.section.course)
-        redirect_to course_path(current_lesson.section.course), :alert => 'Enroll the course to view lesson' 
+      unless current_user.enrolled_in?(current_lesson_course)
+        redirect_to course_path(current_lesson_course), :alert => 'Enroll the course to view lesson' 
       end
   end
 
@@ -11,5 +11,9 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def current_lesson_course
+    lesson_course ||= current_lesson.section.course
   end
 end
